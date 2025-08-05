d4b356c2dc0f1743e27ed30ff7155ec36f1af35f
OsamuChuuya <osamuchuuya26@gmail.com>
2025-08-05 16:01:34 +0300

    Fix import path for Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from './components/header.jsx'
+import Header from './components/Header.jsx'
 import Section1 from './components/Section1.jsx'
 import Section2 from './components/Section2.jsx'
 import Footer from './components/Footer.jsx'
